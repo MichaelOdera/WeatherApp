938f234674a817b2e68a505c1227824f13545ed8
MichaelOdera <michogelira@gmail.com>
2021-05-03 15:03:26 +0300

    generated a class Cloud.java as part of the models that are to be fetched and which are associated with the CityWeatherModel.java class, and inserted it in the models package

---
+++ app/src/main/java/com/michael/weatherapp/models/Clouds.java
@@ -0,0 +1,39 @@
+
+package com.michael.weatherapp.models;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Clouds {
+
+    @SerializedName("all")
+    @Expose
+    private Integer all;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Clouds() {
+    }
+
+    /**
+     * 
+     * @param all
+     */
+    public Clouds(Integer all) {
+        super();
+        this.all = all;
+    }
+
+    public Integer getAll() {
+        return all;
+    }
+
+    public void setAll(Integer all) {
+        this.all = all;
+    }
+
+}
