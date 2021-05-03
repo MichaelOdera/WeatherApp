fcf1089cadfa377dd46f80ce44b51a442b700e48
MichaelOdera <michogelira@gmail.com>
2021-05-03 15:23:22 +0300

    included a main activity MainActivity.java that displays the recyclerview for the cities that are to be displayed in the application's views

---
+++ app/src/main/java/com/michael/weatherapp/ui/MainActivity.java
@@ -0,0 +1,63 @@
+package com.michael.weatherapp.ui;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.LayoutManager;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+
+import com.michael.weatherapp.R;
+import com.michael.weatherapp.adapters.CitiesListAdapter;
+import com.michael.weatherapp.citylistdatasource.CityList;
+import com.michael.weatherapp.models.Main;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+@SuppressLint("NonConstantResourceId")
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> fetchedCitiesListArray;
+    CityList mCityList;
+
+    @BindView(R.id.citiesRecyclerView) RecyclerView mCitiesRecyclerView;
+    private CitiesListAdapter mCitiesListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        mCityList = new CityList();
+        fetchedCitiesListArray = mCityList.getCityList();
+        System.out.println("My fetched cities list ______ +++++ _______" +fetchedCitiesListArray.toString());
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_app_bar);
+
+
+
+
+        View view = getSupportActionBar().getCustomView();
+
+        inflateRecyclerView();
+
+    }
+
+    private void inflateRecyclerView() {
+        LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        mCitiesListAdapter = new CitiesListAdapter(fetchedCitiesListArray, MainActivity.this);
+        mCitiesRecyclerView.setLayoutManager(layoutManager);
+        mCitiesRecyclerView.setAdapter(mCitiesListAdapter);
+        mCitiesRecyclerView.setHasFixedSize(true);
+
+    }
+}
