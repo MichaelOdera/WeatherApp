963650a4b8bfee500e7d49d33edd3e1cfeae0fce
MichaelOdera <michogelira@gmail.com>
2021-05-03 15:13:44 +0300

    added the constant variables for the base url and the open weather map api key that is used through the retrofit instance to fetch the data from the open weather map api

---
+++ app/src/main/java/com/michael/weatherapp/network/NetworkConstants.java
@@ -0,0 +1,8 @@
+package com.michael.weatherapp.network;
+
+import com.michael.weatherapp.BuildConfig;
+
+public class NetworkConstants {
+    public static final String BASE_URL = "https://api.openweathermap.org/data/2.5/";
+    public static String WEATHER_MAP_API_KEY = BuildConfig.WEATHER_MAP_API_KEY;
+}
